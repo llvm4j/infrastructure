791428c74d37b99ed6a2bbc0b1201596a2b7ee5e
supergrecko <me@supergrecko.com>
2021-02-23 02:29:30 +0100

    test(deploy-to-s3): create test harness base (#1)
    Implements a basic Jest test to test the GitHub action. The codebase has also been modularized to make it a lot easier to test.

--- .github/actions/deploy-to-s3/src/action.ts
+++ .github/actions/deploy-to-s3/src/action.ts
@@ -1,101 +1,27 @@
 import * as core from '@actions/core'
-import * as aws from 'aws-sdk'
-import * as mime from 'mime-types'
-import * as fs from 'fs'
-import glob from 'fast-glob'
+import winston from 'winston'
+import { run, DeployConfig } from './aws'
 
 const input = (key: string, required: boolean = true) => core.getInput(key, { required })
-const normalize = (path: string) => {
-  while (path.startsWith('/')) {
-    path = path.substring(1)
-  }
-  while (path.endsWith('/')) {
-    path = path.substring(0, path.length - 1)
-  }
-  return `/${path}`
+
+const configuration: DeployConfig = {
+  accessKey: input('aws-access-key'),
+  secretAccessKey: input('aws-secret-access-key'),
+  bucket: input('aws-bucket'),
+  region: input('aws-region'),
+  directory: input('directory'),
+  prefix: input('prefix', false) ?? null,
+  deleteObjects: input('delete-existing-objects') === 'true'
 }
 
-interface Config {
-  accessKey: string
-  secretAccessKey: string
-  bucket: string
-  region: string
-  directory: string
-  deleteObjects: boolean
-  prefix: string | null
-}
-
-;(async () => {
-  const configuration: Config = {
-    accessKey: input('aws-access-key'),
-    secretAccessKey: input('aws-secret-access-key'),
-    bucket: input('aws-bucket'),
-    region: input('aws-region'),
-    directory: input('directory'),
-    prefix: input('prefix', false) ?? null,
-    deleteObjects: input('delete-existing-objects') === 'true'
-  }
-
-  const s3 = new aws.S3({
-    credentials: {
-      accessKeyId: configuration.accessKey,
-      secretAccessKey: configuration.secretAccessKey
-    },
-    region: configuration.region,
-    apiVersion: '2006-03-01'
-  })
-
-  // Set the "relative root" of where to upload our objects
-  const objectRoot = configuration.prefix !== null
-    ? normalize(configuration.prefix)
-    : '/'
-
-  // Delete all the existing objects if true in configuration
-  if (configuration.deleteObjects) {
-    const nextObjects = () => s3.listObjects({
-      Bucket: configuration.bucket,
-      Prefix: objectRoot
-    }).promise()
-
-    let objects = await nextObjects()
-    // Keep deleting until there are no more items to delete with the prefix
-    while ((objects.Contents?.length ?? 0) > 0) {
-      await s3.deleteObjects({
-        Bucket: configuration.bucket,
-        Delete: {
-          Objects: objects.Contents!.map(obj => ({ Key: obj.Key! }))
-        }
-      }).promise()
-
-      objects = await nextObjects()
-    }
-  }
-
-  const files = await glob(`${configuration.directory}/**`, {
-    followSymbolicLinks: false,
-    onlyFiles: true
-  })
-
-  await Promise.all(
-    files.map(async file => {
-      const objectKey = `${objectRoot}${file}`
-      let mimeType: string | undefined | false = mime.lookup(file)
-      if (mimeType === false) {
-        mimeType = undefined
-      }
-
-      const fileStream = fs.createReadStream(file)
-      fileStream.on('error', (err) => {
-        core.error(`Error creating stream of ${file}: ${err.name} - ${err.message}`)
-      })
-
-      return await s3.upload({
-        Bucket: configuration.bucket,
-        Body: fileStream,
-        ACL: 'public-read',
-        Key: objectKey,
-        ContentType: mimeType
-      }).promise()
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple()
     })
-  )
-})()
+  ]
+})
+
+run(configuration, logger).catch(core.error)
---
+++ .github/actions/deploy-to-s3/src/aws.ts
@@ -0,0 +1,101 @@
+import * as aws from 'aws-sdk'
+import * as mime from 'mime-types'
+import fs from 'fs'
+import glob from 'fast-glob'
+import winston from 'winston'
+import { normalize } from './util'
+
+export interface DeployConfig {
+  accessKey: string
+  secretAccessKey: string
+  bucket: string
+  region: string
+  directory: string
+  deleteObjects: boolean
+  prefix: string | null
+}
+
+export async function run(configuration: DeployConfig, logger: winston.Logger) {
+  const s3 = new aws.S3({
+    credentials: {
+      accessKeyId: configuration.accessKey,
+      secretAccessKey: configuration.secretAccessKey
+    },
+    region: configuration.region,
+    apiVersion: '2006-03-01'
+  })
+
+  // Set the "relative root" of where to upload our objects
+  const root = configuration.prefix !== null
+    ? normalize(configuration.prefix)
+    : ''
+  logger.info(`uploading new objects with a prefix of: ${root}`)
+
+  // Delete all the existing objects if true in configuration
+  if (configuration.deleteObjects) {
+    logger.info('attempting to delete old objects in bucket (delete-existing objects: true)')
+    const getObjects = async () => await s3.listObjectsV2({
+      Bucket: configuration.bucket,
+      Prefix: root
+    }).promise()
+
+    let objects = await getObjects()
+    let objectCount = objects.Contents?.length ?? 0
+
+    if (objectCount === 0) {
+      logger.info('prefix space is empty, no objects to delete')
+    }
+
+    // Keep deleting until there are no more items to delete with the prefix
+    while ((objects.Contents?.length ?? 0) > 0){
+      objects.Contents?.forEach(object => {
+        logger.info(`deleting object ${object.Key}`)
+      })
+      await s3.deleteObjects({
+        Bucket: configuration.bucket,
+        Delete: {
+          Objects: objects.Contents!.map(({ Key }) => ({ Key: Key! }))
+        }
+      }).promise()
+
+      objects = await getObjects()
+      objectCount += objects.Contents?.length ?? 0
+      logger.info(`batch deleted ${objects.Contents?.length ?? 0} objects from bucket`)
+    }
+    logger.info(`${objectCount} objects from bucket have been deleted`)
+  }
+
+  const files = await glob(`${configuration.directory}/**`, {
+    followSymbolicLinks: false,
+    onlyFiles: true
+  })
+
+  await Promise.all(
+    files.map(async file => {
+      logger.info(`uploading file: ${file}`)
+
+      const objectKey = `${root}${file}`
+      let mimeType: string | undefined | false = mime.lookup(file)
+      if (mimeType === false) {
+        mimeType = undefined
+      }
+
+      const fileStream = fs.createReadStream(file)
+      fileStream.on('error', (err) => {
+        logger.error(`error while creating read stream for ${file}: ${err.name} - ${err.message}`)
+      })
+
+      return await s3.upload({
+        Bucket: configuration.bucket,
+        Body: fileStream,
+        ACL: 'public-read',
+        Key: objectKey,
+        ContentType: mimeType
+      }).promise()
+    })
+  )
+
+  logger.info('upload complete')
+
+  return s3
+}
